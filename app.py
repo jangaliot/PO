6b2bb69046d923d8d09e6be06a03380db01124b4
Jan Galiot <86967765+jangaliot@users.noreply.github.com>
2025-07-08 11:21:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ tickers = [
 
 # Función para descargar y procesar datos
 def load_data():
-    end_date = datetime(2025, 6, 1)
+    end_date = datetime(2025, 6, 4)
     start_date = end_date - timedelta(days=3*365)
     test_start_date = end_date - timedelta(days=365)
     
